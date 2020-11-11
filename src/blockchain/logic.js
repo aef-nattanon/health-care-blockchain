46131d35996dd7e4cfe6090f01f190ea2ba8582f
nattanon <nattanon@hospital-os.com>
2020-11-11 11:53:59 +0700

    Add login.js

---
+++ src/blockchain/logic.js
@@ -0,0 +1,24 @@
+const handleBlockchainResponse = (message, getLatestBlock, blockchain, broadcast, responseLatestMsg, replaceChain) => {
+  const receivedBlocks = JSON.parse(message.data).sort((b1, b2) => (b1.index - b2.index));
+  const latestBlockReceived = receivedBlocks[receivedBlocks.length - 1];
+  const latestBlockHeld = getLatestBlock();
+  if (latestBlockReceived.index > latestBlockHeld.index) {
+    console.log('blockchain possibly behind. We got: ' + latestBlockHeld.index + ' Peer got: ' + latestBlockReceived.index);
+    if (latestBlockHeld.hash === latestBlockReceived.previousHash) {
+      console.log("We can append the received block to our chain");
+      blockchain.push(latestBlockReceived);
+      broadcast(responseLatestMsg());
+    } else if (receivedBlocks.length === 1) {
+      console.log("We have to query the chain from our peer");
+      broadcast(queryAllMsg());
+    } else {
+      console.log("Received blockchain is longer than current blockchain");
+      replaceChain(receivedBlocks);
+    }
+  } else {
+    console.log('received blockchain is not longer than current blockchain. Do nothing');
+  }
+};
+
+
+export default { handleBlockchainResponse }
